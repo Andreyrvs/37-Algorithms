7fdaa1e9a841addbd32049b988067527ee2992f2
Andrey Visniewski <cortaesai@gmail.com>
2023-01-26 19:09:46 -0300

    chore: Req 01 - logica do algoritimo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,16 +1,19 @@
 def study_schedule(permanence_period, target_time):
-    if target_time == "" and verify_tuple(permanence_period) is False:
-        return None
 
-    count_student = 0
-    for item in permanence_period:
-        print('item: ', item)
-        for element in item:
-            if item[element] == target_time:
-                count_student += 1
-                print('element: ', element)
+    n = len(permanence_period)
 
-    return count_student
+    for index in range(0, n):
+        for element in permanence_period:
+            if target_time == "" and verify_tuple(element) is False:
+                return None
+
+            print('element: ', element)
+
+            if permanence_period[index] == target_time:
+                print('2: ', index)
+                return index
+
+    return None
 
 
 def verify_tuple(item):
@@ -20,3 +23,17 @@ def verify_tuple(item):
         isinstance(item[1], int),
         isinstance(item[2], int),
     ))
+
+
+    # if target_time == "" and verify_tuple(permanence_period) is False:
+    #     return None
+
+    # count_student = 0
+    # for item in permanence_period:
+    #     print('item: ', item)
+    #     for element in item:
+    #         if item[element] == target_time:
+    #             count_student += 1
+    #             print('element: ', element)
+
+    # return count_student
