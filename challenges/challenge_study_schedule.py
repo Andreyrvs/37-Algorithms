b2b4b5b843700440cc6d8dbe136d9097e94ce9eb
Andrey Visniewski <cortaesai@gmail.com>
2023-01-26 19:01:29 -0300

    chore: Req 01 inicia logia do algoritimo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,9 +2,15 @@ def study_schedule(permanence_period, target_time):
     if target_time == "" and verify_tuple(permanence_period) is False:
         return None
 
+    count_student = 0
     for item in permanence_period:
+        print('item: ', item)
         for element in item:
-            print(element)
+            if item[element] == target_time:
+                count_student += 1
+                print('element: ', element)
+
+    return count_student
 
 
 def verify_tuple(item):
