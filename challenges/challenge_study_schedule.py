2730672de997dce5bedc032020e1f873adf06e52
Andrey Visniewski <cortaesai@gmail.com>
2023-01-26 18:31:45 -0300

    Feat: (Req 01) - Adiciona função que verifica o parametro permanence_period corresponde a uma tupla

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui III."""
-    raise NotImplementedError
+    if target_time == "" and verify_tuple(permanence_period) is False:
+        return None
+
+
+def verify_tuple(item):
+    return all((
+        isinstance(item, tuple),
+        len(item) == 2,
+        isinstance(item[1], int),
+        isinstance(item[2], int),
+    ))
