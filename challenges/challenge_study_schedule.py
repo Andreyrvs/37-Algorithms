33bab8de202a756762d120265db75beccf32fc9e
Andrey Visniewski <cortaesai@gmail.com>
2023-02-01 18:42:16 -0300

    chore: (Requisito 01)problema com a funcao verify_tuple

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,23 +1,21 @@
 def study_schedule(permanence_period, target_time):
     student_qty = 0
-    if target_time is None:
+    if target_time is None or target_time == "":
         return None
     for element in permanence_period:
-        if verify_tuple(element):
-            return None
+        verify_tuple(element)
         if element[0] <= target_time <= element[1]:
             student_qty += 1
-    print('student_qty: ', student_qty)
 
     return student_qty
 
 
 def verify_tuple(item):
-    print('item: ', item)
-    if item is None:
+    if (item is None
+            or len(item) != 2
+            or type(item) != tuple):
         return None
-    if (len(item) != 2
-            or type(item) != tuple
-            or item[1] != int
-            or item[2] != int):
-        return None
+    for invalid in item:
+        if invalid != int:
+            return None
+    return item
