60be157aaa62cea27c89f4d55e6025849cf9571f
Andrey Visniewski <cortaesai@gmail.com>
2023-02-01 17:42:53 -0300

    chore: (Requisito 01)oall

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,17 +1,10 @@
 def study_schedule(permanence_period, target_time):
-
     n = len(permanence_period)
 
     for index in range(0, n):
         for element in permanence_period:
             if target_time == "" and verify_tuple(element) is False:
                 return None
-
-            print('element: ', element)
-
-            if permanence_period[index] == target_time:
-                print('2: ', index)
-                return index
 
     return None
 
@@ -23,17 +16,3 @@ def verify_tuple(item):
         isinstance(item[1], int),
         isinstance(item[2], int),
     ))
-
-
-    # if target_time == "" and verify_tuple(permanence_period) is False:
-    #     return None
-
-    # count_student = 0
-    # for item in permanence_period:
-    #     print('item: ', item)
-    #     for element in item:
-    #         if item[element] == target_time:
-    #             count_student += 1
-    #             print('element: ', element)
-
-    # return count_student
