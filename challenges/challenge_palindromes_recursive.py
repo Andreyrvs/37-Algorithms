7cf72ec308e9af3cbf3584b7db31d29f8586d5d1
Andrey Visniewski <cortaesai@gmail.com>
2023-02-01 20:47:15 -0300

    feat: (Requisito 03) - Adicona funcão auxiliar

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,16 +1,17 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    print('high_index: ', high_index)
-    print('low_index: ', low_index)
-    print('word: ', word)
-    if word == "":
-        return False
-
-    word = word.lower()
-    if high_index < 2:
+    n = len(word)
+    if n == 0:
         return True
-    elif range(word[low_index]) == range(word[high_index - 1]):
-        return is_palindrome_recursive(
-            word[1: high_index - 1], low_index, high_index
-            )
-    else:
+
+    return is_palc_rec(word, low_index, high_index - 1)
+
+
+def is_palc_rec(word, low_index, high_index):
+    if low_index == high_index:
+        return True
+
+    if (word[low_index] != word[high_index]):
         return False
+    if low_index < high_index + 1:
+        return is_palc_rec(word, low_index + 1, high_index + 1)
+    return True
