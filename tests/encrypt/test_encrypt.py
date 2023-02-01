dd5efca2151ca7855e2e08b8157ee316ab6e5277
Andrey Visniewski <cortaesai@gmail.com>
2023-02-01 19:42:40 -0300

    chore: (Requisito 02) - reverse mode

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,10 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(5, 4)
+        encrypt_message("abacate", "tomate")
+    assert encrypt_message("🔥🌊⛱🗿⛱🌊", 4) == "🌊⛱_🗿⛱🌊🔥"
+    # assert encrypt_message("🔥🌊⛱🗿⛱🌊", 0) == "a"
