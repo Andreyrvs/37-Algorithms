066127a74192092235cd1e94608ca833e7d3dc3c
Andrey Visniewski <cortaesai@gmail.com>
2023-02-01 19:50:29 -0300

    chore: (Requisito 02) - Usa numeros pares e impares

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -5,6 +5,9 @@ import pytest
 def test_encrypt_message():
     with pytest.raises(TypeError):
         encrypt_message(5, 4)
-        encrypt_message("abacate", "tomate")
+        encrypt_message(3, "🔥⛱🍷🗿🍷⛱")
+        encrypt_message("🔥⛱🍷🗿🍷⛱", "🔥⛱🍷🗿🍷⛱")
     assert encrypt_message("🔥🌊⛱🗿⛱🌊", 4) == "🌊⛱_🗿⛱🌊🔥"
-    # assert encrypt_message("🔥🌊⛱🗿⛱🌊", 0) == "a"
+    assert encrypt_message("🔥🌊⛱🗿⛱🌊", 0) == "🌊⛱🗿⛱🌊🔥"
+    assert encrypt_message("🌊⛱🍷🗿🍷⛱", 9) == "⛱🍷🗿🍷⛱🌊"
+
