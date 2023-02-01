ba20499821fa715ae17067505a2ca05a1dca46e3
Andrey Visniewski <cortaesai@gmail.com>
2023-02-01 19:09:47 -0300

    fix: (Requisito 01) - Muda a forma como é utilizado a funcao veriry_tuple, é feita a verificacao do retorno dentro do loop da study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,21 +1,16 @@
 def study_schedule(permanence_period, target_time):
     student_qty = 0
-    if target_time is None or target_time == "":
+    if target_time is None:
         return None
     for element in permanence_period:
-        verify_tuple(element)
+        test = verify_tuple(element)
+        if not test:
+            return None
         if element[0] <= target_time <= element[1]:
             student_qty += 1
 
     return student_qty
 
 
-def verify_tuple(item):
-    if (item is None
-            or len(item) != 2
-            or type(item) != tuple):
-        return None
-    for invalid in item:
-        if invalid != int:
-            return None
-    return item
+def verify_tuple(element):
+    return all(isinstance(item, int) for item in element)
