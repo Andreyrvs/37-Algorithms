0950c87a7cf0c38871cead1c35dfa7c77e9846a3
Andrey Visniewski <cortaesai@gmail.com>
2023-02-01 20:23:40 -0300

    feat: (Requisito 03)

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,16 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    print('high_index: ', high_index)
+    print('low_index: ', low_index)
+    print('word: ', word)
+    if word == "":
+        return False
+
+    word = word.lower()
+    if high_index < 2:
+        return True
+    elif range(word[low_index]) == range(word[high_index - 1]):
+        return is_palindrome_recursive(
+            word[1: high_index - 1], low_index, high_index
+            )
+    else:
+        return False
