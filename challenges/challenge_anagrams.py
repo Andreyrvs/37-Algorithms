b9cffb0a5ab8155f3cec9bcfd9862c4c2beb0648
Andrey Visniewski <cortaesai@gmail.com>
2023-02-01 21:30:17 -0300

    chore: (Requisito 04) - retorna uma tupla na funcao

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,6 @@
 def is_anagram(first_string, second_string):
     if first_string == "" or second_string == "":
         return (first_string, second_string, False)
-    return True
+    if len(first_string) != len(second_string):
+        return (first_string, second_string, False)
+    return (first_string, second_string)
