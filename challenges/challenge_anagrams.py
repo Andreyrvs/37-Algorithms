ae07573fdf3c34235ce0f541d808ba70f95c2df0
Andrey Visniewski <cortaesai@gmail.com>
2023-02-01 21:21:59 -0300

    feat: (Requisito 04) - Retorna uma tupla

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,8 +1,4 @@
 def is_anagram(first_string, second_string):
-    print('first_string: ', first_string)
-    print('second_string: ', second_string)
     if first_string == "" or second_string == "":
-        return False
-    if len(first_string) == 0 or len(second_string) == 0:
-        return False
+        return (first_string, second_string, False)
     return True
