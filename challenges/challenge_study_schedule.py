1b05372f6afdee8f0c5b76174dd335fd02773cc4
Andrey Visniewski <cortaesai@gmail.com>
2023-02-01 21:02:31 -0300

    fix: (Requisito 01) - Remove funcao auxiliar: verify_tuple  na tentativa de reduzir o tempo da complexidade do algoritmo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,14 +3,9 @@ def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
     for element in permanence_period:
-        test = verify_tuple(element)
-        if not test:
+        if not all(isinstance(item, int) for item in element):
             return None
         if element[0] <= target_time <= element[1]:
             student_qty += 1
 
     return student_qty
-
-
-def verify_tuple(element):
-    return all(isinstance(item, int) for item in element)
