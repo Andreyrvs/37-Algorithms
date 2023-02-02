20b687578bbde1f734dcef59d157966f5a153fd6
Andrey Visniewski <cortaesai@gmail.com>
2023-02-01 21:06:49 -0300

    fix: (Requisito 01) - altera condicoes dentro do loop visando melhoria de desenpenho

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,9 +3,10 @@ def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
     for element in permanence_period:
-        if not all(isinstance(item, int) for item in element):
+        if all(isinstance(item, int) for item in element):
+            if element[0] <= target_time <= element[1]:
+                student_qty += 1
+        else:
             return None
-        if element[0] <= target_time <= element[1]:
-            student_qty += 1
 
     return student_qty
