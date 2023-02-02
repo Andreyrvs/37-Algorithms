4414184e59f7c058a7983f140e17a49b33d8703b
Andrey Visniewski <cortaesai@gmail.com>
2023-02-02 10:20:07 -0300

    chore: (Requisito 04) - implementa o algoritmo buble sort

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -3,4 +3,23 @@ def is_anagram(first_string, second_string):
         return (first_string, second_string, False)
     if len(first_string) != len(second_string):
         return (first_string, second_string, False)
-    return (first_string, second_string)
+
+    list_1 = list(first_string.lower())
+    list_2 = list(second_string.lower())
+    bubleSort(list_1)
+    bubleSort(list_2)
+    # print(bubleSort(list_1), bubleSort(list_2))
+    string1 = ''.join(list_1)
+    string2 = ''.join(list_2)
+    result = bubleSort(list_1) == bubleSort(list_2)
+    return (string1, string2, result)
+
+
+def bubleSort(array):
+    n = len(array)
+    for ordered_elements in range(n - 1):
+        for item in range(n - ordered_elements - 1):
+            if array[item] > array[item + 1]:
+                array[item], array[item + 1] = array[item + 1], array[item]
+
+    return array
